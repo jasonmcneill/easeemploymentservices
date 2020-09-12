643a68ea354042e4def930df17796783e2981e27
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-09-12 16:09:58 -0700

    Use same route, but different HTTP verb

--- routes/controllers/login.js
+++ routes/controllers/login.js
@@ -1,3 +1,16 @@
 exports.GET = (req, res) => {
   return res.render("login", {});
 };
+
+exports.POST = (req, res) => {
+  const db = require("../../database");
+  const username = req.username;
+  const password = req.password;
+
+  res.status(404).send({
+    msg: "authentication failed",
+    msgType: "error",
+    refreshToken: "",
+    accessToken: "",
+  });
+};
