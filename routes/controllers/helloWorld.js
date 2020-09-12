5c3f6d153d2f3176d164b264bbc9b269b55d7883
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-09-11 17:41:04 -0700

    Remove helloworld API route

--- routes/controllers/helloWorld.js
+++
@@ -1,5 +0,0 @@
-exports.GET = (req, res) => {
-  res.json({
-    hello: "world",
-  });
-};
--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,4 @@ const authenticateToken = utils.authenticateToken;
 const homePage = require("./controllers/homePage");
 router.get("/", homePage.GET);
 
-const helloWorld = require("./controllers/helloWorld");
-router.get("/hello-world", helloWorld.GET);
-
 module.exports = router;
