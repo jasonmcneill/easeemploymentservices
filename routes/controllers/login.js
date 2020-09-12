948ff9e4f5dc8ec5046177e2b64c2466b0aeabfa
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-09-12 16:55:24 -0700

    Verify password with bcrypt

--- routes/controllers/login.js
+++ routes/controllers/login.js
@@ -8,7 +8,39 @@ exports.POST = (req, res) => {
   const saltRounds = 10;
   const username = req.username;
   const password = req.password;
+  const sql = "SELECT id, password FROM employees WHERE username = ? LIMIT 1;";
+  db.query(sql, [username], (err, result) => {
+    if (err)
+      return res.status(500).send({
+        msg: "unable to query for user",
+        msgType: "error",
+      });
 
+    if (result.length === 0)
+      return res.status(404).send({
+        msg: "invalid login",
+        msgType: "error",
+      });
+
+    bcrypt.compare(password, result.password, (err, result) => {
+      if (err)
+        return res.status(500).send({
+          msg: "unable to verify password",
+          msgType: "error",
+        });
+
+      if (result === false)
+        return res.status(400).send({
+          msg: "invalid login",
+          msgType: "error",
+        });
+
+      return res.status(200).send({
+        msg: "user authenticated",
+        msgType: "success",
+      });
+    });
+  });
   res.status(404).send({
     msg: "authentication failed",
     msgType: "error",
