e5a0f8d3396b718f8e26b59e02649cccbe22af55
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-09-21 13:31:06 -0700

    Fix bugs

--- routes/controllers/register.js
+++ routes/controllers/register.js
@@ -1,5 +1,3 @@
-const { sendEmail } = require("../utils");
-
 exports.POST = (req, res) => {
   const db = require("../../database");
   const firstname = req.body.firstname || "";
@@ -11,6 +9,7 @@ exports.POST = (req, res) => {
   const password = req.body.password || "";
   let protocol;
   let host;
+
   switch (process.env.ENV) {
     case "development":
       protocol = "http:";
@@ -40,69 +39,94 @@ exports.POST = (req, res) => {
   if (!password.length)
     return res.status(400).send({ msg: "missing password", msgType: "error" });
 
-  // Validate SMS phone
-  if (smsphone.length) {
-    const validatedPhone = require("../utils").validatePhone(
-      smsphone,
-      smsphonecountry
-    );
-    if (!validatedPhone.isPossibleNumber)
-      return res
-        .status(400)
-        .send({ msg: "mobile phone number not valid", msgType: "error" });
-    if (!validatedPhone.isValidForRegion)
-      return res.status(400).send({
-        msg: "mobile phone number not valid for country",
-        msgType: "error",
-      });
-    if (!validatedPhone.isValidSmsType)
-      return res.status(400).send({
-        msg: "mobile phone number is not sms capable",
-        msgType: "error",
-      });
-    smsphone = validatedPhone.nationalFormat;
-  }
+  // Check eligibility to register (key on e-mail address)
+  const sql = "SELECT employeeid FROM employees WHERE email = ? LIMIT 1;";
+  db.query(sql, [email], (eligibilityErr, eligibilityResult) => {
+    let isEligibleToRegister = false;
+    const defaultUsers = require("../../defaults.json");
 
-  // Check DB for duplicate usernames
-  const sql = "SELECT employeeid FROM employees WHERE username = ? LIMIT 1;";
-  db.query(sql, [username], (err, result) => {
-    if (err)
+    if (eligibilityErr) {
+      console.log(eligibilityErr);
       return res
         .status(500)
-        .send({ msg: "unable to query for username", msgType: "error" });
-    if (result.length === 1)
-      return res
-        .status(400)
-        .send({ msg: "username is taken", msgType: "error" });
+        .send({ msg: "unable to query for eligibility", msgType: "error" });
+    }
 
-    // Validate password complexity
-    const isValidPassword = require("../utils").validateNewPassword(password);
-    if (!isValidPassword)
-      return res.status(400).send({
-        msg: "password lacks sufficient complexity",
-        msgType: "error",
+    if (eligibilityResult.length === 0) {
+      defaultUsers.users.sysadmins.forEach((user) => {
+        if (user.email === email) isEligibleToRegister = true;
       });
 
-    // Check DB for duplicate emails
-    const sql = "SELECT employeeid FROM employees WHERE email = ? LIMIT 1;";
-    db.query(sql, [email], (err, result) => {
-      if (err)
+      defaultUsers.users.directors.forEach((user) => {
+        if (user.email === email) isEligibleToRegister = true;
+      });
+
+      if (!isEligibleToRegister) {
+        return res
+          .status(400)
+          .send({ msg: "not eligible to register", msgType: "error" });
+      }
+    } else {
+      return res
+        .status(400)
+        .send({ msg: "e-mail address is taken", msgType: "error" });
+    }
+
+    // Validate SMS phone
+    if (smsphone.length) {
+      const validatedPhone = require("../utils").validatePhone(
+        smsphone,
+        smsphonecountry
+      );
+      if (!validatedPhone.isPossibleNumber)
+        return res
+          .status(400)
+          .send({ msg: "mobile phone number not valid", msgType: "error" });
+      if (!validatedPhone.isValidForRegion)
+        return res.status(400).send({
+          msg: "mobile phone number not valid for country",
+          msgType: "error",
+        });
+      if (!validatedPhone.isValidSmsType)
+        return res.status(400).send({
+          msg: "mobile phone number is not sms capable",
+          msgType: "error",
+        });
+      smsphone = validatedPhone.nationalFormat;
+    }
+
+    // Check DB for duplicate usernames
+    const sql = "SELECT employeeid FROM employees WHERE username = ? LIMIT 1;";
+    db.query(sql, [username], (err, result) => {
+      if (err) {
+        console.log(err);
         return res
           .status(500)
-          .send({ msg: "unable to query for e-mail", msgType: "error" });
+          .send({ msg: "unable to query for username", msgType: "error" });
+      }
       if (result.length === 1)
         return res
           .status(400)
-          .send({ msg: "e-mail address is taken", msgType: "error" });
+          .send({ msg: "username is taken", msgType: "error" });
+
+      // Validate password complexity
+      const isValidPassword = require("../utils").validateNewPassword(password);
+      if (!isValidPassword)
+        return res.status(400).send({
+          msg: "password lacks sufficient complexity",
+          msgType: "error",
+        });
 
       // Check DB for duplicate SMS phone numbers
       const sql =
         "SELECT employeeid FROM employees WHERE smsphone = ? LIMIT 1;";
       db.query(sql, [smsphone], (err, result) => {
-        if (err)
+        if (err) {
+          console.log(err);
           return res
             .status(500)
             .send({ msg: "unable to query for sms phone", msgType: "error" });
+        }
         if (result.length === 1)
           return res
             .status(400)
@@ -110,7 +134,6 @@ exports.POST = (req, res) => {
 
         // Determine user type
         let usertype = "regular";
-        const defaultUsers = JSON.parse(require("../../defaults.json"));
         defaultUsers.users.sysadmins.forEach((item) => {
           if (item.email === email) role = "sysadmin";
         });
@@ -123,17 +146,21 @@ exports.POST = (req, res) => {
 
         // Hash the password
         bcrypt.genSalt(saltRounds, (err, salt) => {
-          if (err)
+          if (err) {
+            console.log(err);
             return res.status(500).send({
               msg: "unable to generate password salt",
               msgType: "error",
             });
-          bcrypt.hash(newPassword, salt, (err, hash) => {
-            if (err)
+          }
+          bcrypt.hash(password, salt, (err, hash) => {
+            if (err) {
+              console.log(err);
               return res.status(500).send({
                 msg: "unable to generate password hash",
                 msgType: "error",
               });
+            }
 
             // Insert the new record
             const moment = require("moment");
@@ -166,11 +193,13 @@ exports.POST = (req, res) => {
                 createdAt,
               ],
               (err, result) => {
-                if (err)
+                if (err) {
+                  console.log(err);
                   return res.status(500).send({
                     msg: "unable to insert new user",
                     msgType: "error",
                   });
+                }
 
                 const insertedEmployeeId = result.insertId;
 
@@ -199,54 +228,57 @@ exports.POST = (req, res) => {
                   sql,
                   [registrationToken, expiry, insertedEmployeeId, createdAt],
                   (err, result) => {
-                    if (err)
+                    if (err) {
+                      console.log(err);
                       return res.status(500).send({
                         msg: "unable to insert registration token",
                         msgType: "error",
                       });
+                    }
 
-                    const sendEmail = require("../utils").sendEmail;
+                    const utils = require("../utils");
+                    const sendEmail = utils.sendEmail;
                     const messageID = require("uuid").v4();
                     const confirmationUrl = `${protocol}//${host}/register-confirm/#token=${registrationToken}`;
                     const recipient = `${firstname} ${lastname} <${email}>`;
                     const sender = `E.A.S.E. <no-reply@access.easeemploymentservices.com>`;
                     const subject = "Confirm your registration";
                     const body = `
-                    <p>
-                      This message is for ${firstname} ${lastname}. In order to complete your registration, please click on the link below:
-                    </p>
-                    <p style="margin: 30px 0">
-                      <strong><big>
-                        <a href="${confirmationUrl}" style="text-decoration: underline">
-                          Confirm my registration
-                        </a>
-                      </big></strong>
-                    </p>
-                    <p>E.A.S.E. Employment Services</p>
-                    <div style="margin: 40px 0 20px 0">
-                      <small><small style="color: #ccc">
-                        <hr size="1" color="#ccc" />
-                        Message ID: ${messageID.toUpperCase()}
-                      </small></small>
-                    </div>
+                      <p>
+                        This message is for ${firstname} ${lastname}. In order to complete your registration, please click on the link below:
+                      </p>
+                      <p style="margin: 30px 0">
+                        <strong><big>
+                          <a href="${confirmationUrl}" style="text-decoration: underline">
+                            Confirm my registration
+                          </a>
+                        </big></strong>
+                      </p>
+                      <p>E.A.S.E. Employment Services</p>
+                      <div style="margin: 40px 0 20px 0">
+                        <small><small style="color: #ccc">
+                          <hr size="1" color="#ccc" />
+                          Message ID: ${messageID.toUpperCase()}
+                        </small></small>
+                      </div>
                   `;
+
+                    sendEmail(recipient, sender, subject, body)
+                      .then((result) => {
+                        return res.status(result[0].statusCode || 200).send({
+                          msg: "confirmation e-mail sent",
+                          msgType: "success",
+                        });
+                      })
+                      .catch((error) => {
+                        console.log(require("util").inspect(error, 7, true, 7));
+                        return res.status(500).send({
+                          msg: "confirmation e-mail could not be sent",
+                          msgType: "error",
+                        });
+                      });
                   }
                 );
-
-                sendEmail(recipient, sender, subject, body)
-                  .then((result) => {
-                    return res.status(result[0].statusCode || 200).send({
-                      msg: "confirmation e-mail sent",
-                      msgType: "success",
-                    });
-                  })
-                  .catch((error) => {
-                    console.log(error);
-                    return res.status(500).send({
-                      msg: "confirmation e-mail could not be sent",
-                      msgType: "error",
-                    });
-                  });
               }
             );
           });
@@ -254,11 +286,4 @@ exports.POST = (req, res) => {
       });
     });
   });
-
-  // TODO:  Populate DB with registration token & expiry
-  // TODO:  Send e-mail with registration token
-
-  return res
-    .status(200)
-    .send({ msg: "confirmation e-mail sent", msgType: "success" });
 };
