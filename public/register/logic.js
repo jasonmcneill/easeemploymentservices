0ba747b5a0cb190d36ca43813c3688d76f75d94c
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-09-21 13:30:40 -0700

    Handle API responses

--- public/register/logic.js
+++ public/register/logic.js
@@ -7,7 +7,7 @@ function onSubmit(e) {
   const smsphonecountry = e.target["smsphonecountry"].value;
   const username = e.target["username"].value.trim();
   const password = e.target["password"].value.trim();
-  const endpoint = `${window.location.protocol}//${window.location.host}/register`;
+  const endpoint = "/register";
   const spinner = document.querySelector("#registerSpinner");
   const content = document.querySelector("#registerContent");
 
@@ -58,11 +58,15 @@ function onSubmit(e) {
           );
           break;
         case "mobile phone number not valid":
-          showError("Please use a valid phone number.", "Form Incomplete");
+          showError(
+            "<div class='text-center'>Please use a valid phone number.</div>",
+            "Form Incomplete"
+          );
           break;
         case "mobile phone number not valid for country":
           showError(
-            "Please use a mobile phone number that matches the country you selected."
+            "Please use a mobile phone number that matches the country you selected.",
+            "Incorrect Country"
           );
           break;
         case "mobile phone number is not sms capable":
@@ -83,11 +87,109 @@ function onSubmit(e) {
             "Form Incomplete"
           );
           break;
-
+        case "unable to query for eligibility":
+          showError(
+            "<div class='text-center'>It appears that the database is not accessible.  Please wait a moment then try again.</div>",
+            "Database is Down"
+          );
+          break;
+        case "not eligible to register":
+          showError(
+            "In order to register, your e-mail address must be pre-approved and input into the system in advance.  Please arrange this with your direct report, then try registering again.",
+            "Not Eligible"
+          );
+          break;
+        case "e-mail address is taken":
+          showError(
+            "<div class='text-center'>Your e-mail address is already being used by a registered user.</div>",
+            "E-mail in Use"
+          );
+          break;
+        case "mobile phone number not valid":
+          showError(
+            "<div class='text-center'>Please input a valid mobile phone number.</div>",
+            "Invalid Phone Number"
+          );
+          break;
+        case "mobile phone number not valid for country":
+          showError(
+            "Please select the country that correctly corresponds to your mobile phone number.",
+            "Invalid Phone Country"
+          );
+          break;
+        case "mobile phone number is not sms capable":
+          showError(
+            "Please use a mobile phone number that is capable of receiving text messages.",
+            "Invalid Phone Number"
+          );
+          break;
+        case "unable to query for username":
+          showError(
+            "It appears that the database is not accessible.  Please wait a moment then try again.",
+            "Database is Down"
+          );
+          break;
+        case "username is taken":
+          showError(
+            "The username that you have provided is already being used by a registered user.",
+            "Username in Use"
+          );
+          break;
+        case "password lacks sufficient complexity":
+          showError(
+            `
+              <p>Please revise your password.  For your protection, we require that it must have a minimum level of unpredictability in order to prevent potential hackers from guessing it.</p>
+              <p>A minimum length of 8 characters is required.  Using a phrase (rather than a single word) is encouraged, because phrases are less predictable.
+            `,
+            "Password is Too Predictable"
+          );
+          break;
+        case "unable to query for sms phone":
+          showError(
+            "It appears that the database is not accessible.  Please wait a moment then try again.",
+            "Database is Down"
+          );
+          break;
+        case "sms phone is taken":
+          showError(
+            "<div class='text-center'>Your mobile phone number is already being used by a registered user.</div>",
+            "Phone Number in Use"
+          );
+          break;
+        case "unable to generate password salt":
+          showError(
+            "There was a technical glitch which prevented us from processing your registration.  Please wait a moment, then try registering 0again.",
+            "System is Down"
+          );
+          break;
+        case "unable to generate password hash":
+          showError(
+            "There was a technical glitch which prevented us from processing your registration.  Please wait a moment, then try registering again.",
+            "System is Down"
+          );
+          break;
+        case "unable to insert new user":
+          showError(
+            "It appears that the database is not accessible.  Please wait a moment then try again.",
+            "Database is Down"
+          );
+          break;
+        case "unable to insert registration token":
+          showError(
+            "It appears that the database is not accessible.  Please wait a moment then try again.",
+            "Database is Down"
+          );
+          break;
+        case "confirmation e-mail could not be sent":
+          showError(
+            "There was a technical glitch which prevented us from processing your registration.  Please wait a moment, then try registering again.",
+            "System is Down"
+          );
+          break;
         case "confirmation e-mail sent":
           document.querySelector("#registerform").classList.add("d-none");
           showSuccess(
-            "Please check your e-mail.  We have sent you a link that you must click on in order to complete and confirm your registration.",
+            "Please check your e-mail.  We have sent you a link that you must click on in order to confirm your e-mail address and complete your registration.",
             "Check Your E-mail"
           );
           break;
