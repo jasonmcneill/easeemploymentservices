85605f0fae699ed5200c507d40d29b122d8c6e68
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-03 11:03:28 -0700

    Restore SW

--- public/_assets/js/global.js
+++ public/_assets/js/global.js
@@ -268,8 +268,8 @@ function showToast(
   showToasts();
 }
 
-/* if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js");
   });
-} */
+}
