9f26344bb6995ba13522894dbdd8c71ca43727b6
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-03 12:15:58 -0700

    Enforce role authorization

--- public/employees/logic.js
+++ public/employees/logic.js
@@ -1,91 +1,83 @@
-function listEmployees() {
-  const endpoint = "/api/employee/employees-list";
+async function listEmployees() {
+  const content = await getContent();
+  populateContent(content);
+}
 
-  function populateContent(data) {
-    if (Array.isArray(data)) {
-      const el = document.querySelector("#employeelist");
-      let employees = document.createElement("div");
-      data.forEach((item) => {
-        const { firstname, lastname, status } = item;
-        let employee = "";
-
-        if (status !== "pending") {
-          employee = `
-            <a href="profile/#${item.employeeid}" class="list-group-item list-group-item-action">
-              ${firstname} ${lastname}
-            </a>
-          `;
-        } else {
-          employee = `
-            <a href="profile/#${item.employeeid}" class="list-group-item list-group-item-action list-group-item-secondary">
-              ${firstname} ${lastname}
-              <span class="badge badge-pill badge-secondary ml-2 float-right">
-                unregistered
-              </span>
-            </a>
-          `;
-        }
-
-        employees.innerHTML += employee;
-      });
-      if (el.innerHTML !== employees.innerHTML) {
-        el.innerHTML = employees.innerHTML;
-      }
-    }
-  }
-
-  async function getContent() {
-    const spinner = document.querySelector("#spinner");
-    const content = document.querySelector("#employeelist");
-    const accessToken = await getAccessToken();
-
-    showSpinner(content, spinner);
-    fetch(endpoint, {
-      method: "GET",
-      mode: "cors",
-      headers: new Headers({
-        "Content-Type": "application/json",
-        authorization: `Bearer ${accessToken}`,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        localforage.setItem(endpoint, data).then(() => populateContent(data));
-      })
-      .catch((error) => {
-        console.error(error);
-        if (!navigator.onLine) {
-          return showError(
-            "You appear to be offline. Please connect to the internet, then reload the page.",
-            "No Connection"
-          );
-        }
-      })
-      .finally(() => {
-        hideSpinner(content, spinner);
-      });
-  }
-
+async function getContent() {
   const spinner = document.querySelector("#spinner");
   const content = document.querySelector("#employeelist");
-  localforage
-    .getItem(endpoint)
+  const accessToken = await getAccessToken();
+  const endpoint = "/api/employee/employees-list";
+
+  showSpinner(content, spinner);
+  fetch(endpoint, {
+    method: "GET",
+    mode: "cors",
+    headers: new Headers({
+      "Content-Type": "application/json",
+      authorization: `Bearer ${accessToken}`,
+    }),
+  })
+    .then((res) => res.json())
     .then((data) => {
-      if (data && data.length) {
-        populateContent(data);
-      } else {
-        showSpinner(content, spinner);
+      switch (data.msg) {
+        case "user is not authorized for this action":
+          addToast(
+            "Your account does not have sufficient permissions to perform that action.",
+            "Not Authorized",
+            "danger"
+          );
+          window.location.href = "/";
+          break;
       }
-    })
-    .then(() => {
-      getContent();
+
+      localforage.setItem(endpoint, data).then(() => populateContent(data));
     })
     .catch((error) => {
       console.error(error);
+      if (!navigator.onLine) {
+        return showError(
+          "You appear to be offline. Please connect to the internet, then reload the page.",
+          "No Connection"
+        );
+      }
     })
     .finally(() => {
       hideSpinner(content, spinner);
     });
+}
+
+function populateContent(data) {
+  if (Array.isArray(data)) {
+    const el = document.querySelector("#employeelist");
+    let employees = document.createElement("div");
+    data.forEach((item) => {
+      const { firstname, lastname, status } = item;
+      let employee = "";
+
+      if (status !== "pending") {
+        employee = `
+          <a href="profile/#${item.employeeid}" class="list-group-item list-group-item-action">
+            ${firstname} ${lastname}
+          </a>
+        `;
+      } else {
+        employee = `
+          <a href="profile/#${item.employeeid}" class="list-group-item list-group-item-action list-group-item-secondary">
+            ${firstname} ${lastname}
+            <span class="badge badge-pill badge-secondary ml-2 float-right">
+              unregistered
+            </span>
+          </a>
+        `;
+      }
+
+      employees.innerHTML += employee;
+    });
+    if (el.innerHTML !== employees.innerHTML) {
+      el.innerHTML = employees.innerHTML;
+    }
+  }
 }
 
 function init() {
--- routes/controllers/employees_list.js
+++ routes/controllers/employees_list.js
@@ -4,6 +4,18 @@ exports.GET = (req, res) => {
   const sql =
     "SELECT employeeid, firstname, lastname, status, type FROM employees ORDER BY lastname, firstname;";
 
+  // Enforce authorization
+  const usertype = req.user.type;
+  const allowedUsertypes = ["sysadmin", "director"];
+  if (!allowedUsertypes.includes(usertype)) {
+    console.log(`User (employeeid ${req.user.employeeid} is not authorized.`);
+    return res.status(401).send({
+      msg: "user is not authorized for this action",
+      msgType: "error",
+    });
+  }
+
+  // Query
   db.query(sql, [], (err, results) => {
     if (err) {
       console.log(err);
