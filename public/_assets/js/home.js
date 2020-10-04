61d3baf7c1e28ae4c9b0ac9b2f1d0a996327ca79
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-03 17:15:58 -0700

    Add time entry card

--- public/_assets/js/home.js
+++ public/_assets/js/home.js
@@ -2,10 +2,20 @@ function showParticipantsChart() {
   const chartParticipants = document.getElementById("chartParticipants");
 }
 
+function populateClockTime() {
+  const clockTime = document.querySelector("#clocktime");
+
+  setInterval(() => {
+    const currentTime = new Date().toLocaleTimeString();
+    clockTime.textContent = currentTime;
+  }, 1000);
+}
+
 function init() {
   protectRoute();
   // showParticipantsChart();
   showToasts();
+  populateClockTime();
 }
 
 init();
