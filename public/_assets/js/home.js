b2fb3039b407262741f15689534b79f3015dfaa4
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-04 15:02:21 -0700

    Update time entry UI

--- public/_assets/js/home.js
+++ public/_assets/js/home.js
@@ -1,19 +1,57 @@
-function showParticipantsChart() {
-  const chartParticipants = document.getElementById("chartParticipants");
-}
-
 function populateClockTime() {
   const clockTime = document.querySelector("#clocktime");
+  const clockDate = document.querySelector("#clockdate");
 
   setInterval(() => {
-    const currentTime = new Date().toLocaleTimeString();
-    clockTime.textContent = currentTime;
+    const dateObj = new Date();
+    const currentDate = dateObj.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    const currentTime = dateObj.toLocaleTimeString();
+    clockDate.innerHTML = `${currentDate}`;
+    clockTime.innerHTML = `<h3 class="h3">${currentTime}</h3>`;
   }, 1000);
+}
+
+function onClockInClicked() {}
+
+function onClockOutClicked() {}
+
+function onDoneForTheDayClicked(e) {
+  const clockIn = document.querySelector("#btnClockIn");
+  const clockOut = document.querySelector("#btnClockOut");
+  const doneForTheDayContainer = document.querySelector(
+    "#doneForTheDayContainer"
+  );
+  const checked = e.target.checked || false;
+
+  if (checked) {
+    clockIn.classList.add("d-none");
+    clockOut.classList.add("d-none");
+    doneForTheDayContainer.classList.add("d-none");
+  }
+}
+
+function attachListeners() {
+  document
+    .querySelector("#btnClockIn")
+    .addEventListener("click", onClockInClicked);
+
+  document
+    .querySelector("#btnClockOut")
+    .addEventListener("click", onClockOutClicked);
+
+  document
+    .querySelector("#doneForTheDay")
+    .addEventListener("click", onDoneForTheDayClicked);
 }
 
 function init() {
   protectRoute();
-  // showParticipantsChart();
+  attachListeners();
   showToasts();
   populateClockTime();
 }
