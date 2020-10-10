b4301a04b3c393865e86da0f703e76315dd8767b
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-09 19:32:46 -0700

    Fix "To" date being 1 day too far ahead

--- routes/controllers/timeentries_employee.js
+++ routes/controllers/timeentries_employee.js
@@ -24,6 +24,7 @@ exports.POST = (req, res) => {
     todate = moment
       .utc()
       .subtract(timeZoneOffset, "hours")
+      .subtract(1, "days")
       .format("YYYY-MM-DD 23:59:59");
   } else {
     todate = moment(req.body.todate).format("YYYY-MM-DD 23:59:59");
