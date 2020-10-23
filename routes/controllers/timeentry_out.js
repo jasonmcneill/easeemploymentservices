b204eb9b76ac7831209b6b6d78855d4db2c6fd81
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-23 09:06:20 -0700

    Fix bug with missing participantid

--- routes/controllers/timeentry_out.js
+++ routes/controllers/timeentry_out.js
@@ -3,17 +3,33 @@ const db = require("../../database");
 
 exports.POST = (req, res) => {
   const employeeid = req.user.employeeid;
-  const participantid = req.body.participantid;
   const timeZone = req.body.timeZone;
   const timeZoneOffset = moment.tz(timeZone).format("Z:00").slice(0, -3);
 
-  const sql = `
+  const sql =
+    "SELECT participantid FROM employees__timelogs WHERE employeeid = ? ORDER BY timelogid DESC LIMIT 1;";
+  db.query(sql, [employeeid], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .send({ msg: "unable to query for participantid", msgType: "error" });
+    }
+
+    let participantid = 0;
+    if (result.length === 1) {
+      participantid = result[0].participantid;
+    }
+
+    const sql = `
     INSERT INTO employees__timelogs (
       employeeid,
+      participantid,
       entry,
       type,
       createdAt
     ) VALUES (
+      ?,
       ?,
       UTC_TIMESTAMP(),
       'out',
@@ -21,15 +37,15 @@ exports.POST = (req, res) => {
     )
   ;`;
 
-  db.query(sql, [employeeid], (err, result1) => {
-    if (err) {
-      console.log(err);
-      return res
-        .status(500)
-        .send({ msg: "unable to insert time entry", msgType: "error" });
-    }
+    db.query(sql, [employeeid, participantid], (err, result1) => {
+      if (err) {
+        console.log(err);
+        return res
+          .status(500)
+          .send({ msg: "unable to insert time entry", msgType: "error" });
+      }
 
-    const sql = `
+      const sql = `
       SELECT
         convert_tz(t.entry, '+00:00', ?) AS entry,
         t.type,
@@ -48,44 +64,45 @@ exports.POST = (req, res) => {
       ;
     `;
 
-    db.query(
-      sql,
-      [
-        timeZoneOffset,
-        timeZoneOffset,
-        timeZoneOffset,
-        timeZoneOffset,
-        timeZoneOffset,
-        employeeid,
-      ],
-      (err, result2) => {
-        if (err) {
-          console.log(err);
-          return res.status(500).send({
-            msg: "unable to query for time entries",
-            msgType: "error",
+      db.query(
+        sql,
+        [
+          timeZoneOffset,
+          timeZoneOffset,
+          timeZoneOffset,
+          timeZoneOffset,
+          timeZoneOffset,
+          employeeid,
+        ],
+        (err, result2) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).send({
+              msg: "unable to query for time entries",
+              msgType: "error",
+            });
+          }
+
+          const entries = result2.map((item) => {
+            const { type, entry, participantid, firstname, lastname } = item;
+            const changedItem = {
+              type: type,
+              entry: moment(entry).format("h:mm:ss A"),
+              participant: {
+                id: participantid,
+                name: participantid === 0 ? "EASE" : `${firstname} ${lastname}`,
+              },
+            };
+            return changedItem;
+          });
+
+          return res.status(200).send({
+            msg: "clock-out succeeded",
+            msgType: "success",
+            entries: entries,
           });
         }
-
-        const entries = result2.map((item) => {
-          const { type, entry, participantid, firstname, lastname } = item;
-          const changedItem = {
-            type: type,
-            entry: moment(entry).format("h:mm:ss A"),
-            participant: {
-              id: participantid,
-              name: participantid === 0 ? "EASE" : `${firstname} ${lastname}`,
-            },
-          };
-          return changedItem;
-        });
-
-        return res.status(200).send({
-          msg: "clock-out succeeded",
-          msgType: "success",
-          entries: entries,
-        });
-      }
-    );
+      );
+    });
   });
 };
