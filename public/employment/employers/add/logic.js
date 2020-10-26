8969132a7eee944ae0617f93709109ab687e32e3
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-25 21:22:21 -0700

    Handle errors

--- public/employment/employers/add/logic.js
+++ public/employment/employers/add/logic.js
@@ -1,5 +1,6 @@
 function loadCountries() {
-  const endpoint = "../../_assets/json/world-countries/data/en/countries.json";
+  const endpoint =
+    "../../../_assets/json/world-countries/data/en/countries.json";
   const countriesEl = document.querySelector("#phonecountry");
 
   fetch(endpoint)
@@ -21,7 +22,7 @@ function loadCountries() {
 }
 
 function loadStates() {
-  const endpoint = "../../_assets/json/states.json";
+  const endpoint = "../../../_assets/json/states.json";
   const stateEl = document.querySelector("#state");
 
   fetch(endpoint)
@@ -50,13 +51,21 @@ async function onSubmit(e) {
   const content = document.querySelector("#content");
   const endpoint = "/api/employer-add";
 
+  let website = e.target["website"].value.trim().toLowerCase();
+  if (
+    website.substr(0, 7) !== "http://" &&
+    website.substr(0, 8) !== "https://"
+  ) {
+    website = `http://${website}`;
+  }
+
   showSpinner(content, spinner);
   fetch(endpoint, {
     mode: "cors",
     method: "POST",
     body: JSON.stringify({
       companyname: e.target["companyname"].value.trim(),
-      website: e.target["website"].value.trim().toLowerCase(),
+      website: website,
       phone: e.target["phone"].value.trim(),
       phonecountry: e.target["phonecountry"].value,
       address: e.target["address"].value.trim(),
@@ -73,55 +82,137 @@ async function onSubmit(e) {
     .then((data) => {
       const employerid = data.employerid || "";
 
-      hideSpinner(content, spinner);
       switch (data.msg) {
         case "user is not authorized for this action":
+          addToast(
+            "You do not have sufficient permissions for that action.",
+            "Not Authorized",
+            "danger"
+          );
           window.location.href = "/";
           break;
         case "missing company name":
-          showError("", "Form Incomplete");
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please input the company name.</div>",
+            "Form Incomplete"
+          );
           break;
         case "invalid website":
-          showError("", "Form Incomplete");
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please check the Web site address for accuracy and proper formatting.</div>",
+            "Form Incomplete"
+          );
           break;
         case "missing phone":
-          showError("", "Form Incomplete");
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please input the phone number.</div>",
+            "Form Incomplete"
+          );
+          break;
+        case "missing phone country":
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please select the country for the phone number.</div>",
+            "Form Incomplete"
+          );
           break;
         case "missing address":
-          showError("", "Form Incomplete");
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please input the address.</div>",
+            "Form Incomplete"
+          );
           break;
         case "missing city":
-          showError("", "Form Incomplete");
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please input the city.</div>",
+            "Form Incomplete"
+          );
           break;
         case "missing state":
-          showError("", "Form Incomplete");
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please input the state.</div>",
+            "Form Incomplete"
+          );
           break;
         case "missing zip":
-          showError("", "Form Incomplete");
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please input the ZIP.</div>",
+            "Form Incomplete"
+          );
           break;
         case "unable to query for duplicate company name":
-          showError("", "Database is Down");
+          hideSpinner(content, spinner);
+          showError(
+            "There was a technical glitch preventing the system from adding the company.  Please wait a moment then try again.",
+            "Database is Down"
+          );
           break;
         case "company name already exists":
-          showError("", "Duplicate");
+          hideSpinner(content, spinner);
+          showError(
+            `This company is <a href="../profile/#${data.employerid}" class="alert-link">already in the system</a>.`,
+            "Duplicate"
+          );
           break;
         case "unable to query for duplicate phone number":
-          showError("", "Database is Down");
+          hideSpinner(content, spinner);
+          showError(
+            "There was a technical glitch preventing the system from adding the company.  Please wait a moment then try again.",
+            "Database is Down"
+          );
           break;
         case "phone number already exists":
-          showError("", "Duplicate");
+          hideSpinner(content, spinner);
+          showError(
+            `This company is <a href="../profile/#${data.employerid}" class="alert-link">already in the system</a>.`,
+            "Duplicate"
+          );
           break;
         case "unable to query for duplicate website":
-          showError("", "Database is Down");
+          hideSpinner(content, spinner);
+          showError(
+            "There was a technical glitch preventing the system from adding the company.  Please wait a moment then try again.",
+            "Database is Down"
+          );
           break;
         case "website already exists":
-          showError("", "Duplicate");
+          hideSpinner(content, spinner);
+          showError(
+            `This company is <a href="../profile/#${data.employerid}" class="alert-link">already in the system</a>.`,
+            "Duplicate"
+          );
+          break;
+        case "invalid phone number":
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>Please check the phone number for accuracy and completeness.</div>",
+            "Invalid phone number"
+          );
+          break;
+        case "invalid phone number for region":
+          hideSpinner(content, spinner);
+          showError(
+            "<div class='text-center'>The phone number you entered is not valid in the country specified.</div>",
+            "Phone Invalid for Country"
+          );
           break;
         case "unable to insert new employer":
-          showError("", "Database is Down");
+          hideSpinner(content, spinner);
+          showError(
+            "There was a technical glitch preventing the system from adding the company.  Please wait a moment then try again.",
+            "Database is Down"
+          );
           break;
         case "employer added successfully":
-          showSuccess("", "Employer Added Successfully");
+          addToast("The employer was added successfully.", "Employer Added");
+          window.location.href = "../";
           break;
       }
     })
