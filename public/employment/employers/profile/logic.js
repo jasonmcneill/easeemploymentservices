9c825f90cba60453712c458981121724b0cd0431
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-25 21:22:30 -0700

    Initial commit

---
+++ public/employment/employers/profile/logic.js
@@ -0,0 +1,159 @@
+async function getEmployer() {
+  const employerid = parseInt(document.location.hash.split("#")[1]) || "";
+  const accessToken = await getAccessToken();
+  const endpoint = `/api/employer/${employerid}`;
+  const content = document.querySelector("#content");
+  const spinner = document.querySelector("#spinner");
+
+  if (typeof employerid !== "number") {
+    addToast("Invalid employer ID", "Can't show employer", "danger");
+    window.location.href = "../";
+  }
+
+  showSpinner(content, spinner);
+  fetch(endpoint, {
+    mode: "cors",
+    method: "GET",
+    headers: new Headers({
+      "Content-Type": "application/json",
+      authorization: `Bearer ${accessToken}`,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      hideSpinner(content, spinner);
+      switch (data.msg) {
+        case "invalid employer id":
+          addToast("Invalid employer ID", "Can't show employer", "danger");
+          window.location.href = "../";
+          break;
+        case "unable to query for employer":
+          showError(
+            "There was a technical glitch preventing this employer from being displayed.  Please wait a moment then reload the page.",
+            "Database is Down"
+          );
+          break;
+        case "employer retrieved":
+          showEmployer(data.data);
+          break;
+      }
+    })
+    .catch((err) => {
+      hideSpinner(content, spinner);
+      console.error(err);
+    });
+}
+
+function showEmployer(data) {
+  const { companyname, website, phone, address, city, state, zip } = data;
+
+  document.querySelectorAll("[data-name]").forEach((item) => {
+    item.innerHTML = companyname;
+  });
+
+  document.querySelector(
+    "#website"
+  ).innerHTML = `<a href="${website}">${website}</a>`;
+  document.querySelector("#phone").innerHTML = phone;
+  document.querySelector(
+    "#address"
+  ).innerHTML = `${address}<br>${city}, ${state} ${zip}`;
+}
+
+function onEdit(e) {
+  e.preventDefault();
+  console.log("Edit");
+}
+
+function onDelete(e) {
+  e.preventDefault();
+  $("#modalDeleteEmployer").modal("show");
+}
+
+async function onConfirmDelete(e) {
+  const employerid = parseInt(document.location.hash.split("#")[1]) || "";
+  const accessToken = await getAccessToken();
+  const endpoint = "/api/employer-delete";
+  const content = document.querySelector("#content");
+  const spinner = document.querySelector("#spinner");
+
+  e.preventDefault();
+
+  // Validate
+  if (typeof employerid !== "number") {
+    $("#modalDeleteEmployer").modal("hide");
+    return showError(
+      "The ID of the employer could not be determined from the URL to this page, and so the delete could not be processed.",
+      "Could not delete"
+    );
+  }
+
+  $("#modalDeleteEmployer").modal("hide");
+  showSpinner(content, spinner);
+  fetch(endpoint, {
+    mode: "cors",
+    method: "POST",
+    body: JSON.stringify({
+      employerid: employerid,
+    }),
+    headers: new Headers({
+      "Content-Type": "application/json",
+      authorization: `Bearer ${accessToken}`,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      hideSpinner(content, spinner);
+      switch (data.msg) {
+        case "user is not authorized for this action":
+          addToast(
+            "You do not have sufficient permissions to delete an employer.",
+            "Could not delete",
+            "danger"
+          );
+          window.location.href = "../";
+          break;
+        case "invalid employer id":
+          showError(
+            "The ID of the employer could not be determined from the URL to this page, and so the delete could not be processed.",
+            "Could not delete"
+          );
+          break;
+        case "employer deleted":
+          addToast(
+            "The employer was deleted successfully.",
+            "Employer deleted",
+            "success"
+          );
+          window.location.href = "../";
+          break;
+        default:
+          showError(
+            "There was a technical glitch preventing the employer from being deleted.  Please wait a moment then try again.",
+            "Could not delete"
+          );
+          console.error(data.error);
+          break;
+      }
+    })
+    .catch((err) => {
+      hideSpinner(content, spinner);
+      console.error(err);
+    });
+}
+
+function attachListeners() {
+  document.querySelector("#btnEdit").addEventListener("click", onEdit);
+  document.querySelector("#btnDelete").addEventListener("click", onDelete);
+  document
+    .querySelector("#btnConfirmDelete")
+    .addEventListener("click", onConfirmDelete);
+}
+
+function init() {
+  getEmployer();
+  attachListeners();
+  showToasts();
+}
+
+init();
