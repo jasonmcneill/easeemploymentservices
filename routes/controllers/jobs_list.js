d37ddb3187c703983456e116a76f1366fac52b2a
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-10-28 11:19:55 -0700

    Enable listing jobs

---
+++ routes/controllers/jobs_list.js
@@ -0,0 +1,39 @@
+const moment = require("moment-timezone");
+const db = require("../../database");
+
+exports.POST = (req, res) => {
+  const timeZone = req.body.timeZone || "America/Los_Angeles";
+  const timeZoneOffset = moment.tz(timeZone).format("Z:00").slice(0, -3);
+  const sql = `
+    SELECT
+      j.jobid,
+      j.jobtitle,
+      j.city,
+      j.state,
+      convert_tz(j.createdAt, '+00:00', ?) AS createdAt,
+      e.companyname
+    FROM
+      jobs j
+    INNER JOIN
+      employers e
+    ON
+      j.employerid = e.employerid
+    ORDER BY
+      j.createdAt DESC,
+      j.employerid,
+      j.jobtitle
+    ;
+  `;
+  db.query(sql, [timeZoneOffset], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .send({ msg: "unable to query for jobs", msgType: "error" });
+    }
+
+    return res
+      .status(200)
+      .send({ msg: "jobs retrieved", msgType: "success", data: result });
+  });
+};
