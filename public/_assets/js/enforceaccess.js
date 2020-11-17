b7798f61b24840f0d48c04bacd7b73864d82aaf5
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-11-17 12:35:41 -0700

    Enforce user roles in access to views

---
+++ public/_assets/js/enforceaccess.js
@@ -0,0 +1,13 @@
+function verifyRefreshToken(logoutUrl) {
+  const refreshToken = localStorage.getItem("refreshToken") || "";
+  const isAuthorized = refreshToken.length;
+  if (!isAuthorized) window.location.href = logoutUrl;
+}
+
+function enforceUserRoles(allowedRoles, redirectUrl) {
+  const userRole = JSON.parse(
+    atob(sessionStorage.getItem("accessToken").split(".")[1])
+  ).type;
+  const isAuthorized = allowedRoles.includes(userRole);
+  if (!isAuthorized) window.location.href = redirectUrl;
+}
